af8e0a5b9e694e8e241c4d591a3b1aa81bb8ce83
furryfoxyofficial <span.smjr.span@gmail.com>
2025-08-22 08:56:22 +0330

    Nats will cross the all computers

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,11 @@
 using namespace std;
 int main()
 {
+    /*
+     *
+     *
+     *
+     */
     cout << "Hello NATS!\n" << "\nThis is furry foxy official version \
     \nproject of nats.io" << endl;
-
-
-
-    return 0;
 }
