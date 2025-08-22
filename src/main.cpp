51523666770a3bfb830c29ecb9e2f47874f194e8
furryfoxyofficial <span.smjr.span@gmail.com>
2025-08-22 08:01:35 +0330

    tracked

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,10 @@
 using namespace std;
 int main()
 {
-    cout << "Hello NATS!" << endl;
+    cout << "Hello NATS!\n" << "\nThis is furry foxy official version \
+    \nproject of nats.io" << endl;
+
+
+
     return 0;
 }
